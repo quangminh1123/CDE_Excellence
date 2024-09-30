a2e834a258feee051a87616bf934dd32aab6f4a1
minhdqpc06173 <minhdqpc06173@fpt.edu.vn>
2024-09-30 09:08:14 +0700

    Add project API

---
+++ API_CDE/API_CDE/Data/ApplicationDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_CDE.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+    }
+}
